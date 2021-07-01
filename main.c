b5594e1e161ca5c06ff75b30b4a2a66dc25afa22
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2021-07-01 14:04:15 -0300

    It tells the player if his attempt was bigger or smaller than the secret number

--- main.c
+++ main.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 
 int main(void){
+    printf("\n");
     printf("*************************************\n");
     printf("* Bem-vindo ao jogo de Adivinhação! *\n");
-    printf("*************************************\n");
+    printf("*************************************\n\n");
 
     int secret_number = 42;
     int user_attempt;
+    int numbers_are_equal;
 
     printf("Qual é o seu chute? ");
     scanf("%d", &user_attempt);
-    printf("Seu chute foi: %d\n", user_attempt);
+    numbers_are_equal = user_attempt == secret_number;
 
-    if(user_attempt == secret_number)
-        printf("Parabéns, você acertou!\n");
-    else
-        printf("Você errou!\n");
+    if(numbers_are_equal)
+        printf("PARABÉNS, você acertou!\n");
+    else{
+        int smaller = (user_attempt < secret_number);
+
+        if(smaller)
+            printf("O seu chute foi MENOR que o número secreto.\n");
+        else
+            printf("O seu chute foi MAIOR que o número secreto.\n");
+    }
+    printf("\n");
 }
