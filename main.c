b58a2f112e5f3cf3e4917ce19c2bbee69239b447
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2021-07-01 10:06:49 -0300

    It tells the player if his attempt was correct or not

--- main.c
+++ main.c
@@ -11,4 +11,9 @@ int main(void){
     printf("Qual é o seu chute? ");
     scanf("%d", &user_attempt);
     printf("Seu chute foi: %d\n", user_attempt);
+
+    if(user_attempt == secret_number)
+        printf("Parabéns, você acertou!\n");
+    else
+        printf("Você errou!\n");
 }
