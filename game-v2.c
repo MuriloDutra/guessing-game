085660635232b99bc6b21abc4240ad15de405f55
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2021-07-01 20:30:19 -0300

    New version of the code -> 2.0

---
+++ game-v2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(void){
+    printf("\n");
+    printf("*************************************\n");
+    printf("* Bem-vindo ao jogo de Adivinhação! *\n");
+    printf("*************************************\n\n");
+
+    int secret_number = 42;
+    int user_attempt;
+    int numbers_are_equal;
+    int won_the_game = 0;
+    int counter = 1;
+
+    while(!won_the_game){
+        printf("TENTATIVA - %d\n\n", counter);
+        printf("Qual é o seu chute? ");
+        scanf("%d", &user_attempt);
+
+        if(user_attempt < 0){
+            printf("Não é permitido chutar números negativos.\n");
+            continue;
+        }
+
+        numbers_are_equal = user_attempt == secret_number;
+
+        if(numbers_are_equal){
+            printf("PARABÉNS, você acertou!\n");
+            won_the_game = 1;
+        }
+        else{
+            int smaller = (user_attempt < secret_number);
+
+            if(smaller)
+                printf("O seu chute foi MENOR que o número secreto.\n");
+            else
+                printf("O seu chute foi MAIOR que o número secreto.\n");
+        }
+        printf("\n");
+        counter++;
+    }
+
+    printf("Você acertou em %d tentativas.\n", counter - 1);
+}
