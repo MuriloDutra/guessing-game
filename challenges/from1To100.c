715619fe241babcbc4a6242fa4e44fa079cf4cf6
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2021-07-01 20:29:38 -0300

    New challenges

---
+++ challenges/from1To100.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    for(int i = 1; i <= 100; i++){
+        printf("%d\n", i);
+    }
+
+    int counter = 1;
+    while(counter <= 100){
+        printf("%d\n", counter);
+        counter++;
+    }
+}
