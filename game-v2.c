62d302c83f37aec400d4eea36e63e8971722397a
Murilo <MuriloDutraProjects@gmail.com>
2021-07-12 21:56:12 -0300

    User can choose a difficulty level

--- game-v2.c
+++ game-v2.c
@@ -12,12 +12,31 @@ int main(void){
     srand(seconds);
     int secret_number = rand() % 100;
     int user_attempt;
+    int attempts_quantity;
     int numbers_are_equal;
     int won_the_game = 0;
-    int counter = 1;
+    int counter;
+    int difficulty_level;
     double score = 1000;
 
-    while(!won_the_game){
+    printf("Qual o nível de dificuldade?\n");
+    printf("(1) Fácil (2) Médio (3) Difícil\n\n");
+    printf("Escolha: ");
+    scanf("%d", &difficulty_level);
+
+    switch(difficulty_level)
+    {
+        case 1:
+            attempts_quantity = 20;
+            break;
+        case 2:
+            attempts_quantity = 15;
+            break;
+        default:
+            attempts_quantity = 6;
+    }
+
+    for(counter = 1; counter <= attempts_quantity; counter++){
         printf("TENTATIVA - %d\n\n", counter);
         printf("Qual é o seu chute? ");
         scanf("%d", &user_attempt);
@@ -27,12 +46,10 @@ int main(void){
             continue;
         }
 
-        numbers_are_equal = user_attempt == secret_number;
+        numbers_are_equal = (user_attempt == secret_number);
 
-        if(numbers_are_equal){
-            printf("PARABÉNS, você acertou!\n");
-            won_the_game = 1;
-        }
+        if(numbers_are_equal)
+            break;
         else{
             int smaller = (user_attempt < secret_number);
 
@@ -42,13 +59,15 @@ int main(void){
                 printf("O seu chute foi MAIOR que o número secreto.\n");
         }
         printf("\n");
-        counter++;
         double lost_points = (user_attempt - secret_number) / (double) 2;
 
-        score -= (lost_points < 0)
-            ? (-lost_points)
-            : lost_points;
+        score -= (lost_points < 0) ? (-lost_points) : lost_points;
     }
-    printf("Você acertou em %d tentativas.\n", counter - 1);
-    printf("Total de pontos: %.2f\n", score);
+
+    if(numbers_are_equal){
+        printf("Você ganhou, PARABÉNS!\n");
+        printf("Você acertou em %d tentativas.\n", counter);
+        printf("Total de pontos: %.2f\n", score);
+    }else
+        printf("Você perdeu, tente novamente.");
 }
